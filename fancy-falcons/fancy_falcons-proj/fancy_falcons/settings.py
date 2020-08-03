a7f865859a0288268aa8a4b34e0eb879673830fe
Istalantar <istalantar@gmail.com>
2020-08-03 23:29:51 +0200

    implementing login-feature part 1

--- fancy-falcons/fancy_falcons-proj/fancy_falcons/settings.py
+++ fancy-falcons/fancy_falcons-proj/fancy_falcons/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login.apps.LoginConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,5 +122,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
