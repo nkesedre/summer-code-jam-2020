22c5bef3708e16fad70fe2d2fe3ec44eb19a55fc
Istalantar <istalantar@gmail.com>
2020-08-04 22:20:32 +0200

    implementing login-feature part 2 - edit user profile

--- fancy-falcons/fancy_falcons-proj/fancy_falcons/urls.py
+++ fancy-falcons/fancy_falcons-proj/fancy_falcons/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 from login import views as login_views
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('login/', include('login.urls')),
     path('profile/', login_views.profile, name='profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- fancy-falcons/fancy_falcons-proj/login/views.py
+++ fancy-falcons/fancy_falcons-proj/login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You can log in now.')
+            messages.success(request, f'Account {username} has been created! You can log in now.')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -19,4 +19,23 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'login/profile.html')
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated')
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+    }
+
+    return render(request, 'login/profile.html', context)
