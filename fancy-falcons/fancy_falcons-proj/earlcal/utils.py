8fbf2353d0b93b087005b844bba7cf26c0e79144
Istalantar <istalantar@gmail.com>
2020-08-09 19:16:24 +0200

    corrected flake8 errors

--- fancy-falcons/fancy_falcons-proj/earlcal/utils.py
+++ fancy-falcons/fancy_falcons-proj/earlcal/utils.py
@@ -15,9 +15,9 @@ class Calendar(HTMLCalendar):
         earls_per_day = earls.filter(birthday__day=day)
         events_per_day = events.filter(start_time__day=day)
 
-		d = ''
-		for earl in earls_per_day:
-			d += f'<br><span class="badge badge-danger">🎂 {earl.get_html_url} </span>'
+        d = ''
+        for earl in earls_per_day:
+            d += f'<br><span class="badge badge-danger">🎂 {earl.get_html_url} </span>'
 
         for event in events_per_day:
             d += f'<br><span class="badge badge-info"> {event.get_html_url} </span>'
@@ -26,23 +26,25 @@ class Calendar(HTMLCalendar):
             return f"<td><span class='date'>{day}</span>{d}</td>"
         return '<td></td>'
 
-    # formats a week as a tr
-    def formatweek(self, theweek, earls, events):
-        week = ''
-        for d, weekday in theweek:
-            week += self.formatday(d, earls, events)
-        return f'<tr> {week} </tr>'
 
-    # formats a month as a table
-    # filter events by year and month
-    def formatmonth(self, withyear=True):
-        earls = Account.objects.filter(birthday__month=self.month)
-        events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
+# formats a week as a tr
+def formatweek(self, theweek, earls, events):
+    week = ''
+    for d, weekday in theweek:
+        week += self.formatday(d, earls, events)
+    return f'<tr> {week} </tr>'
 
-        cal = '<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
-        cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
-        cal += f'{self.formatweekheader()}\n'
-        for week in self.monthdays2calendar(self.year, self.month):
-            cal += f'{self.formatweek(week, earls, events)}\n'
-        cal += '</table>'
-        return cal
+
+# formats a month as a table
+# filter events by year and month
+def formatmonth(self, withyear=True):
+    earls = Account.objects.filter(birthday__month=self.month)
+    events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
+
+    cal = '<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
+    cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
+    cal += f'{self.formatweekheader()}\n'
+    for week in self.monthdays2calendar(self.year, self.month):
+        cal += f'{self.formatweek(week, earls, events)}\n'
+    cal += '</table>'
+    return cal
